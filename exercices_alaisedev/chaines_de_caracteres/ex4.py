a4e9f1d3abcb16c58c9b01dc2c29fe57ee8fdced
lasbl <lasbleis.olivier@yahoo.fr>
2025-07-07 14:13:38 +0200

    second

---
+++ exercices_alaisedev/chaines_de_caracteres/ex4.py
@@ -0,0 +1,12 @@
+
+str1 = "lmi  re aeae aèmiudxae  is o tsq ij?"
+str2 = "eanjmc qt tslpsi re tslsrutipt,usse?"
+str3 =" t,eahàurpt,'r ds upt,eo rrsae iu- ?"
+
+# voici un message codé
+# pour le lire créez un phrase avec successivement
+# la première lettre de str1, la première lettre de str2, la première lettre de str3
+# puis la deuxieme lettre de str1, la deuxieme lettre de str2, la deuxieme lettre de str3
+# et ainsi de suite
+
+
---
+++ exercices_alaisedev/chaines_de_caracteres/ex5.py
@@ -0,0 +1,3 @@
+# coder la phrase suivante selon le procédé de l'exercice précédent
+
+str = "le trésor est dans la cuisine sous le tapis rouge, ouvrir la trappe en activant le chandelier"
